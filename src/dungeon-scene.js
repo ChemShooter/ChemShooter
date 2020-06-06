96e38c33556d7a576712a25ea4eb101cb8724d03
Leon Si <leonzalion@gmail.com>
2020-06-06 09:19:18 -0400

    added tilesets

---
+++ src/dungeon-scene.js
@@ -0,0 +1,154 @@
+import Phaser from 'phaser';
+import Dungeon from '@mikewesthad/dungeon';
+import Tiles from './constants/Tiles';
+import Player from './player';
+import TilemapVisibility from "./tilemap-visibility";
+
+export default class DungeonScene extends Phaser.Scene {
+  constructor() {
+    super({});
+    this.level = 0;
+  }
+
+  preload() {
+    this.load.image('tiles', 'assets/tilesets/dungeon.png');
+    this.load.spritesheet(
+      'characters',
+      'assets/spritesheets/characters.png',
+      {
+        frameWidth: 16,
+        frameHeight: 32
+      }
+    );
+  }
+
+  create() {
+    ++this.level;
+    this.hasPlayerReachedStairs = false;
+
+    this.dungeon = new Dungeon({
+      width: 50,
+      height: 50,
+      doorPadding: 2,
+      rooms: {
+        width: { min: 20, max: 30, onlyEven: true },
+        height: { min: 20, max: 30, onlyEven: true }
+      }
+    });
+
+    this.dungeon.drawToConsole({});
+
+    const map = this.make.tilemap({
+      tileWidth: 16,
+      tileHeight: 16,
+      width: this.dungeon.width,
+      height: this.dungeon.height
+    });
+
+    const tileset = map.addTilesetImage('tiles', null, 16, 16, 0, 0);
+    this.groundLayer = map.createBlankDynamicLayer('Ground', tileset).fill(Tiles.BLANK);
+    this.stuffLayer = map.createBlankDynamicLayer('Stuff', tileset);
+    const shadowLayer = map.createBlankDynamicLayer('Shadow', tileset).fill(Tiles.BLANK);
+
+    this.tilemapVisibility = new TilemapVisibility(shadowLayer);
+
+    for (const room of this.dungeon.rooms) {
+      const { x, y, width, height, left, right, top, bottom } = room;
+
+      this.groundLayer.weightedRandomize(x + 1, y + 1, width - 2, height - 2, Tiles.FLOOR);
+
+      // Placing corner tiles
+      this.groundLayer.putTileAt(Tiles.WALL.TOP_LEFT.TOP_HALF, left, top);
+      this.groundLayer.putTileAt(Tiles.WALL.TOP_LEFT.BOTTOM_HALF, left, top - 1);
+      this.groundLayer.putTileAt(Tiles.WALL.TOP_RIGHT.TOP_HALF, right, top);
+      this.groundLayer.putTileAt(Tiles.WALL.TOP_RIGHT.BOTTOM_HALF, right, top - 1);
+      this.groundLayer.putTileAt(Tiles.WALL.BOTTOM_RIGHT.TOP_HALF, right, bottom + 1);
+      this.groundLayer.putTileAt(Tiles.WALL.BOTTOM_RIGHT.BOTTOM_HALF, right, bottom);
+      this.groundLayer.putTileAt(Tiles.WALL.BOTTOM_LEFT.TOP_HALF, left, bottom + 1);
+      this.groundLayer.putTileAt(Tiles.WALL.BOTTOM_LEFT.BOTTOM_HALF, left, bottom);
+
+      /*
+      // Filling walls with mostly clean tiles, but occasionally place a dirty tile
+      this.groundLayer.weightedRandomize(left + 1, top, width - 2, 1, Tiles.WALL.TOP);
+      this.groundLayer.weightedRandomize(left + 1, bottom, width - 2, 1, Tiles.WALL.BOTTOM);
+      this.groundLayer.weightedRandomize(left, top + 1, 1, height - 2, Tiles.WALL.LEFT);
+      this.groundLayer.weightedRandomize(right, top + 1, 1, height - 2, Tiles.WALL.RIGHT);
+       */
+
+      // Placing the doors that connect the dungeon rooms. The x & y coordinates of the door are relative
+      // to their room
+      const doors = room.getDoorLocations();
+      for (const door of doors) {
+        if (door.y === 0) {
+          this.groundLayer.putTilesAt(Tiles.DOOR.TOP, x + door.x - 1, y + door.y);
+        } else if (door.y === room.height - 1) {
+          this.groundLayer.putTilesAt(Tiles.DOOR.BOTTOM, x + door.x - 1, y + door.y);
+        } else if (door.x === 0) {
+          this.groundLayer.putTilesAt(Tiles.DOOR.LEFT, x + door.x, y + door.y - 1);
+        } else if (door.x === room.width - 1) {
+          this.groundLayer.putTilesAt(Tiles.DOOR.RIGHT, x + door.x, y + door.y - 1);
+        }
+      }
+    }
+
+    const rooms = this.dungeon.rooms.slice();
+    const startRoom = rooms.shift();
+    const endRoom = Phaser.Utils.Array.RemoveRandomElement(rooms);
+    const otherRooms = Phaser.Utils.Array.Shuffle(rooms).slice(0, rooms.length * 0.9);
+
+    this.stuffLayer.putTileAt(Tiles.EXIT, endRoom.centerX, endRoom.centerY);
+
+    for (const room of otherRooms) {
+      const rand = Math.random();
+      if (rand <= 0.25) {
+        // 25% chance of finding a chest
+        this.stuffLayer.putTileAt(Tiles.CHEST, room.centerX, room.centerY);
+      }
+    }
+
+    this.stuffLayer.setTileIndexCallback(Tiles.EXIT, () => {
+      this.stuffLayer.setTileIndexCallback(Tiles.EXIT, null, this);
+      this.hasPlayerReachedStairs = true;
+      this.player.freeze();
+      const cam = this.cameras.main;
+      cam.fade(250, 0, 0, 0);
+      cam.once('camerafadeoutcomplete', () => {
+        this.player.destroy();
+        this.scene.restart();
+      });
+    }, this);
+
+    const playerRoom = startRoom;
+    const x = map.tileToWorldX(playerRoom.centerX);
+    const y = map.tileToWorldY(playerRoom.centerY);
+    this.player = new Player(this, x, y);
+
+    this.physics.add.collider(this.player.sprite, this.groundLayer);
+    this.physics.add.collider(this.player.sprite, this.stuffLayer);
+
+    const camera = this.cameras.main;
+
+    camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+    camera.startFollow(this.player.sprite);
+
+    this.add
+      .text(16, 16, `Find the stairs. Go deeper.\nCurrent level: ${this.level}`, {
+        font: '18px monospace',
+        fill: '#000000',
+        padding: { x: 20, y: 10 },
+        backgroundColor: '#ffffff'
+      })
+      .setScrollFactor(0);
+  }
+
+  update(time, delta) {
+    if (this.hasPlayerReachedStairs) return;
+    this.player.update();
+
+    const playerTileX = this.groundLayer.worldToTileX(this.player.sprite.x);
+    const playerTileY = this.groundLayer.worldToTileY(this.player.sprite.y);
+    const playerRoom = this.dungeon.getRoomAt(playerTileX, playerTileY);
+
+    this.tilemapVisibility.setActiveRoom(playerRoom);
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import Phaser from 'phaser';
+import DungeonScene from './dungeon-scene';
 
 const config = {
   type: Phaser.AUTO, // Which renderer to use
@@ -6,74 +7,13 @@ const config = {
   height: 600, // Canvas height in pixels
   parent: "game-container", // ID of the DOM element to add the canvas to
   pixelArt: true,
-  scene: {
-    preload: preload,
-    create: create,
-    update: update
+  scene: DungeonScene,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 0 }
+    }
   }
 };
 
 const game = new Phaser.Game(config);
-let controls;
-
-function preload() {
-  // Runs once, loads up assets like images and audio
-  this.load.image("tiles", "assets/tilesets/tuxmon-sample-32px-extruded.png");
-  this.load.tilemapTiledJSON("map", "assets/tilemaps/tuxemon-town.json");
-}
-
-function create() {
-  // Runs once, after all assets in preload are loaded
-
-  const map = this.make.tilemap({ key: "map"});
-
-  // Parameters are the name you gave the tileset in Tiled and then the key of the tileset image in
-  // Phaser's cache (i.e. the name you used in preload)
-  const tileset = map.addTilesetImage("tuxmon-sample-32px-extruded", "tiles");
-  // const layer = map.createStaticLayer(0, tileset, 0, 0);
-
-
-  // Phaser supports multiple cameras, but you can access the default camera like this:
-  const camera = this.cameras.main;
-
-  // Parameters: layer name (or index) from Tiled, tileset, x, y
-  const belowLayer = map.createStaticLayer("Below Player", tileset, 0, 0);
-  const worldLayer = map.createStaticLayer("World", tileset, 0, 0);
-  const aboveLayer = map.createStaticLayer("Above Player", tileset, 0, 0);
-
-  worldLayer.setCollisionByProperty({ collides: true });
-
-  const debugGraphics = this.add.graphics().setAlpha(0.75);
-
-  worldLayer.renderDebug(debugGraphics, {
-    tileColor: null, // Color of non-colliding tiles
-    collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
-    faceColor: new Phaser.Display.Color(40, 39, 37, 255) // Color of colliding face edges
-  });
-
-  // Set up the arrows to control the camera
-  const cursors = this.input.keyboard.createCursorKeys();
-  controls = new Phaser.Cameras.Controls.FixedKeyControl({
-    camera: camera,
-    left: cursors.left,
-    right: cursors.right,
-    up: cursors.up,
-    down: cursors.down,
-    speed: 0.5
-  });
-
-  // Constrain the camera so that it isn't allowed to move outside the width/height of tilemap
-  camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
-
-  this.add.text(16, 16, "Arrow keys to scroll", {
-    font: "18px monospace",
-    fill: "#ffffff",
-    padding: {x: 20, y: 10},
-    backgroundColor: "#000000"
-  }).setScrollFactor(0);
-}
-
-function update(time, delta) {
-  // Runs once per frame for the duration of the scene
-  controls.update(delta);
-}
