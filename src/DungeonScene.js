085f75048bc7d8c088da085dc7865e12528d8351
Leon Si <leonzalion@gmail.com>
2020-06-06 21:24:55 -0400

    updated

--- src/DungeonScene.js
+++ src/DungeonScene.js
@@ -34,41 +34,13 @@ export default class DungeonScene extends Phaser.Scene {
       }
     );
     this.load.image('bullet', 'assets/images/bullet.png');
-    this.load.image('healthcontainer', 'assets/images/healthcontainer.png');
-    this.load.image('healthbar', 'assets/images/healthbar.png');
   }
 
   create() {
-    this.level++;
+    ++this.game.playerLevel;
     this.hasPlayerReachedStairs = false;
 
-    var pauseButton = this.add.text(
-      800, 0, '||', {
-          font: "18px monospace",
-          fill: "#000000",
-          padding: { x: 7, y: 7 },
-          backgroundColor: "#cc96ff"
-    })
-    .setScrollFactor(0)
-    .setDepth(3)
-    .setOrigin(1, 0)
-    .on('pointerover', () => mousehover = true)
-    .on('pointerout', () => mousehover = false)
-    .on('pointerdown', () => {
-      this.scene.pause();
-      this.scene.launch('PauseScene');
-    });
-    pauseButton.setInteractive();
-
-
-    const openPauseScene = () => {
-      this.scene.pause();
-      this.scene.launch('PauseScene');
-    }
-
-    this.input.keyboard.on('keydown_ESC', openPauseScene);
-    this.input.keyboard.on('keydown_P', openPauseScene);
-
+    this.overlay = this.scene.launch('OverlayScene', this);
 
     // Generate a random world with a few extra options:
     //  - Rooms should only have odd number dimensions so that they have a center tile.
@@ -247,34 +219,13 @@ export default class DungeonScene extends Phaser.Scene {
       });
     });
 
-
     // Phaser supports multiple cameras, but you can access the default camera like this:
     const camera = this.cameras.main;
+    this.cameras.main.zoom = 2;
 
     // Constrain the camera so that it isn't allowed to move outside the width/height of tilemap
     camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
     camera.startFollow(this.player.sprite);
-
-    const gameWidth = this.game.config.width;
-    const gameHeight = this.game.config.height;
-    const healthContainer = this.add.sprite(0, gameHeight, 'healthcontainer');
-    healthContainer.setDepth(10).setScrollFactor(0).setOrigin(0, 1).setScale(0.4, 0.4);
-    const healthBar = this.add.sprite(healthContainer.x + 46, healthContainer.y - 10, 'healthbar');
-    healthBar.setDepth(11).setScrollFactor(0).setOrigin(0, 1).setScale(0.4, 0.4);
-    this.healthMask = this.add.sprite(healthBar.x, healthBar.y, 'healthbar');
-    this.healthMask.setScrollFactor(0).setOrigin(0, 1).setScale(0.4, 0.4);
-    this.healthMask.visible = false;
-    healthBar.mask = new Phaser.Display.Masks.BitmapMask(this, this.healthMask);
-
-    // Help text that has a "fixed" position on the screen
-    this.add
-      .text(16, 16, `Find the stairs. Go deeper.\nCurrent level: ${this.level}`, {
-        font: "18px monospace",
-        fill: "#000000",
-        padding: { x: 20, y: 10 },
-        backgroundColor: "#ffffff"
-      })
-      .setScrollFactor(0).setDepth(3);
 
     // Bullets stuff
     this.bullets = this.physics.add.group({
---
+++ src/OverlayScene.js
@@ -0,0 +1,96 @@
+import Phaser from "phaser";
+import Dungeon from "@mikewesthad/dungeon";
+import Player from "./Player.js";
+import SkeletEnemy from "./enemies/SkeletEnemy";
+import MuddyEnemy from "./enemies/MuddyEnemy";
+import SwampyEnemy from "./enemies/SwampyEnemy";
+import TILES from "./TileMapping.js";
+import TilemapVisibility from "./TilemapVisibility.js";
+
+/**
+ * Scene that displays the sprites that overlay the map
+ */
+
+// global
+var mousehover=false;
+
+export default class OverlayScene extends Phaser.Scene {
+  constructor() {
+    super({key: 'OverlayScene'});
+  }
+
+  init(dungeonScene) {
+    this.prevHealth = 100;
+    this.dungeonScene = dungeonScene;
+  }
+
+  preload() {
+    this.load.image('healthcontainer', 'assets/images/healthcontainer.png');
+    this.load.image('healthbar', 'assets/images/healthbar.png');
+  }
+
+  create() {
+    var pauseButton = this.add.text(
+      800, 0, '||', {
+        font: "18px monospace",
+        fill: "#000000",
+        padding: {x: 7, y: 7},
+        backgroundColor: "#cc96ff"
+      })
+      .setScrollFactor(0)
+      .setDepth(3)
+      .setOrigin(1, 0)
+      .on('pointerover', () => mousehover = true)
+      .on('pointerout', () => mousehover = false)
+      .on('pointerdown', () => {
+        this.scene.pause();
+        this.scene.launch('PauseScene');
+      });
+    pauseButton.setInteractive();
+
+    const openPauseScene = () => {
+      this.scene.pause();
+      this.scene.launch('PauseScene');
+    }
+
+    this.input.keyboard.on('keydown_ESC', openPauseScene);
+    this.input.keyboard.on('keydown_P', openPauseScene);
+
+    // Phaser supports multiple cameras, but you can access the default camera like this:
+    const camera = this.cameras.main;
+
+    const gameWidth = this.game.config.width;
+    const gameHeight = this.game.config.height;
+
+    const healthContainer = this.add.sprite(0, gameHeight, 'healthcontainer');
+    healthContainer.setDepth(10).setScrollFactor(0).setOrigin(0, 1).setScale(0.4, 0.4);
+    const healthBar = this.add.sprite(healthContainer.x + 46, healthContainer.y - 10, 'healthbar');
+    healthBar.setDepth(11).setScrollFactor(0).setOrigin(0, 1).setScale(0.4, 0.4);
+    this.healthMask = this.add.sprite(healthBar.x, healthBar.y, 'healthbar');
+    this.healthMask.setScrollFactor(0).setOrigin(0, 1).setScale(0.4, 0.4);
+    this.healthMask.visible = false;
+    healthBar.mask = new Phaser.Display.Masks.BitmapMask(this, this.healthMask);
+
+    // Help text that has a "fixed" position on the screen
+    this.add
+      .text(16, 16, `Find the stairs. Go deeper.\nCurrent level: ${this.dungeonScene.level}`, {
+        font: "18px monospace",
+        fill: "#000000",
+        padding: {x: 20, y: 10},
+        backgroundColor: "#ffffff"
+      })
+      .setScrollFactor(0).setDepth(3);
+  }
+
+  update() {
+    if (this.prevHealth === this.game.playerHealth) return;
+    this.prevHealth = this.game.playerHealth;
+    let stepWidth = this.healthMask.displayWidth / 100;
+    console.log(this.game.playerHealth);
+
+    this.healthMask.x -= stepWidth;
+    if (this.game.playerHealth <= 0) {
+      alert("Game over!");
+    }
+  }
+}
